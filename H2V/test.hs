7bf76dbf66e9fb568fcf868c63bfbbd2a4190652
Reuben D'Netto <rdnetto@gmail.com>
2014-03-22 18:32:36 +1100

    Refactored code into separate files

--- H2V/test.hs
+++ H2V/test.hs
@@ -1,2 +1,11 @@
+module Test (f) where
+
 f x a b c = a2 * x + b * x + c where
     a2 = a * a
+
+-- Returns the nth fibonnacci number
+-- Could be implemented using an infinite list, but that would be more painful to compile to Verilog
+-- x0, x1 are accumulators, n counts down to the required index
+fib n = fib' 0 1 n where
+    fib' x0 _ 0 = x0
+    fib' x0 x1 n = fib' x1 (x0 + x1) (n - 1)
