b700339c99ae837039b47846d9afa0949d817d74
Reuben D'Netto <rdnetto@gmail.com>
2014-05-06 20:16:55 +1000

    Added test case for dependency resolution

--- H2V/test.hs
+++ H2V/test.hs
@@ -3,6 +3,12 @@ module Test (f) where
 f x a b c = a2 * x + b * x + c where
     a2 = a * a
 
+f1 x = x + d where
+    a = 1
+    b = 2
+    c = d - a
+    d = b + 2
+
 -- Returns the nth fibonnacci number
 -- Could be implemented using an infinite list, but that would be more painful to compile to Verilog
 -- x0, x1 are accumulators, n counts down to the required index
