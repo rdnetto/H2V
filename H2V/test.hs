5d11cb7fc9f9cd7317b7483653c7d96436a80180
Reuben D'Netto <rdnetto@gmail.com>
2014-06-01 22:53:37 +1000

    Added test case for pattern matching

--- H2V/test.hs
+++ H2V/test.hs
@@ -18,6 +18,18 @@ f2 a b c = z where
     f2b x = x + b
     d = a + b
 
+--pattern matching
+f3 0 = 0
+f3 x = if x < 0
+       then -1
+       else 1
+
+--pattern guards
+f4 x
+    | x < 0     = 0
+    | x > 0     = 1
+    | otherwise = 2
+
 -- Test case for nested, recursive functions
 -- Returns the nth fibonnacci number
 -- Could be implemented using an infinite list, but that would be more painful to compile to Verilog
@@ -25,4 +37,3 @@ f2 a b c = z where
 fib n = fib' 0 1 n where
     fib' x0 _ 0 = x0
     fib' x0 x1 n = fib' x1 (x0 + x1) (n - 1)
-
