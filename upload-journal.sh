765e85e8dc7470191bcca9754359357a644f7b86
Reuben D'Netto <rdnetto@gmail.com>
2014-07-30 23:36:26 +1000

    Updated journal uploading script

--- upload-journal.sh
+++ upload-journal.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/Remote
-tar acf ECE4094-`date --iso`.tar.bz2 ECE4094 --exclude=ECE4094/scratch
-xdg-open "http://moodle.vle.monash.edu/mod/assignment/view.php?id=1461360"
-kmail --subject "FYP Progress Report" --attach ECE4094*.tar.bz2 lindsay.kleeman@monash.edu david.boland@monash.edu
+tar acf ECE4094-`date --iso`.tar.bz2 ECE4094 --exclude=ECE4094/scratch/nios-spike
+xdg-open "http://moodle.vle.monash.edu/mod/assignment/view.php?id=1461361"
+kmail --subject "FYP Progress Report" --attach ECE4094*.tar.bz2 david.boland@monash.edu
 
