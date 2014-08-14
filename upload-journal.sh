5a152f211656c39cc9cda678ebeaeae1e8235cfc
Reuben D'Netto <rdnetto@gmail.com>
2014-08-14 14:34:05 +1000

    BUG FIX: upload script assumed that there were no other tar archives present

--- upload-journal.sh
+++ upload-journal.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd ~/Remote
-tar acf ECE4094-`date --iso`.tar.bz2 ECE4094 --exclude=ECE4094/scratch/nios-spike
+TAR_FILE="ECE4094-`date --iso`.tar.bz2"
+tar acf "$TAR_FILE" ECE4094 --exclude=ECE4094/scratch/nios-spike
 xdg-open "http://moodle.vle.monash.edu/mod/assignment/view.php?id=1461361"
-kmail --subject "FYP Progress Report" --attach ECE4094*.tar.bz2 david.boland@monash.edu
+kmail --subject "FYP Progress Report" --attach "$TAR_FILE" david.boland@monash.edu
 
