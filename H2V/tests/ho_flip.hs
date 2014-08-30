c2fb6d20eb339c54a5744e5553f291b6ebfe1fe1
Reuben D'Netto <rdnetto@gmail.com>
2014-08-30 15:59:33 +1000

    Rewrote test, since we don't support dependency resolution for higher order functions

--- H2V/tests/ho_flip.hs
+++ H2V/tests/ho_flip.hs
@@ -1,4 +1,7 @@
-f5 x = x + (revsub 2 7) + (revsub 9 10) where
 flip :: (a -> b -> c) -> (b -> a -> c)
 flip f = \a b -> f b a
+
+revsub :: Int -> Int
 revsub = flip (-)
+
+f5 x = x + (revsub 2 7) + (revsub 9 10) where
