ec6131a7bf27dda20d59a782567fe5c215d3d4d3
Reuben D'Netto <rdnetto@gmail.com>
2014-09-18 01:01:06 +1000

    Modified uploading script to open folder automatically.

--- upload-journal.sh
+++ upload-journal.sh
@@ -4,4 +4,4 @@ TAR_FILE="ECE4094-`date --iso`.tar.bz2"
 tar acf "$TAR_FILE" ECE4094 --exclude=ECE4094/scratch/nios-spike
 xdg-open "http://moodle.vle.monash.edu/mod/assignment/view.php?id=1784826"
 kmail --subject "FYP Progress Report" --attach "$TAR_FILE" david.boland@monash.edu
-
+xdg-open ..
