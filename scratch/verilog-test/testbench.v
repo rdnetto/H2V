ff14bd0f5d900d1ff66235b594dd460cfc23fddd
Reuben D'Netto <rdnetto@gmail.com>
2014-09-24 20:33:57 +1000

    Testbench of listLiteral

--- scratch/verilog-test/testbench.v
+++ scratch/verilog-test/testbench.v
@@ -1,14 +1,14 @@
 module testbench(input CLOCK_50, input [1:0] KEY, output [7:0] LED);
     reg ready;
-    reg req, lastAck;
+    reg req;
     reg running;
     reg [7:0] reqCount;
     reg [24:0] timer;
 
     listLiteral_37(CLOCK_50, running,
-        2, , 1,
-        12, , 1,
-        30, , 1,
+        2,  1,
+        12, 1,
+        30, 1,
         req, ack, eol, value);
 
     initial running = 0;
@@ -16,14 +16,18 @@ module testbench(input CLOCK_50, input [1:0] KEY, output [7:0] LED);
     assign done = 1;
 
     always @(posedge CLOCK_50) begin
-        lastAck <= ack;
-
         if(running) begin
-            if(reqCount < 5)
-                reqCount <= reqCount + 1;
-            else if(lastAck) begin
-                req <= ~req;
+            if(ack) begin
+                req <= 0;
                 reqCount <= 0;
+
+            end else begin
+                if(reqCount < 5) begin
+                    reqCount <= reqCount + 1;
+                end else begin
+                    reqCount <= 0;
+                    req <= ~req;
+                end
             end
 
         end else begin
@@ -34,6 +38,9 @@ module testbench(input CLOCK_50, input [1:0] KEY, output [7:0] LED);
         timer <= timer + 1;
         if(timer == 0)
             running <= ~running;
+
+        if(timer == 0 && ~running)
+            req <= 1;
     end
 endmodule
 
