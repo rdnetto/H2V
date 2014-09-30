89356cede73b889add51b2dbe69de2d45e826adf
Reuben D'Netto <rdnetto@gmail.com>
2014-10-01 00:46:35 +1000

    Created a second testbench module for scalar tests

--- scratch/verilog-test/testbench.v
+++ scratch/verilog-test/testbench.v
@@ -5,7 +5,46 @@ module testbench(input CLOCK_50, input [1:0] KEY, output [7:0] LED);
     reg [7:0] reqCount;
     reg [24:0] timer;
 
-    dfd_8 dfdUT(CLOCK_50, running, done,
+    dfd_2 dfdUT(CLOCK_50, running, done,
+        1, result);
+
+    initial running = 0;
+    assign LED = 8'd0;
+    assign done = 1;
+
+    always @(posedge CLOCK_50) begin
+        if(running) begin
+				 if(reqCount < 10) begin
+					  reqCount <= reqCount + 1;
+				 end else begin
+					  reqCount <= 0;
+					  req <= ~req;
+				 end
+
+        end else begin
+            req <= 0;
+            reqCount <= 0;
+        end
+
+        timer <= timer + 1;
+        if(timer == 0)
+            running <= ~running;
+
+        if(timer == 0 && ~running)
+            req <= 1;
+    end
+endmodule
+
+
+
+module list_testbench(input CLOCK_50, input [1:0] KEY, output [7:0] LED);
+    reg ready;
+    reg req;
+    reg running;
+    reg [7:0] reqCount;
+    reg [24:0] timer;
+
+    dfd_2 dfdUT(CLOCK_50, running, done,
         1, 2,
         req, ack, value, value_valid);
 
