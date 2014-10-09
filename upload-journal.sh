9bac25d500b89047489313b5cd7e5aa276196fa0
Reuben D'Netto <rdnetto@gmail.com>
2014-10-09 11:39:10 +1100

    Excluded Quartus projects from tarballs, since we're hitting Gmail's size limit.

--- upload-journal.sh
+++ upload-journal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ~/Remote
 TAR_FILE="ECE4094-`date --iso`.tar.bz2"
-tar acf "$TAR_FILE" ECE4094 --exclude=ECE4094/scratch/nios-spike
+tar acf "$TAR_FILE" ECE4094 --exclude=ECE4094/scratch
 xdg-open "http://moodle.vle.monash.edu/mod/assignment/view.php?id=1784826"
 kmail --subject "FYP Progress Report" --attach "$TAR_FILE" david.boland@monash.edu
 xdg-open ..
